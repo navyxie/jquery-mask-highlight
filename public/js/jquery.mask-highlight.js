b9002eb158857e488695b43f1f71d417064b422b
navy <navyxie2010@gmail.com>
2013-04-07 19:34:47 +0800

    empty content

---
+++ public/js/jquery.mask-highlight.js
@@ -0,0 +1,13 @@
+/**
+ * 基于jquery1.7+的遮罩高亮插件
+ * author:navy
+ * email:navyxie2010@gmail.com
+ * qq:951178609
+ * version:1.0 beta
+ * date:2013/04/07
+ */
+var NAVY = NAVY || {};
+NAVY.Drag = function(){
+};
+NAVY.Drag.prototype = {
+};
